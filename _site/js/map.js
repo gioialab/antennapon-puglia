6ba2fed14464717971ebb56c1857e5217ecbb710
Giuseppe Zileni <giuseppe.zileni@gmail.com>
2016-10-31 10:27:39 +0100

    updated

--- _site/js/map.js
+++ _site/js/map.js
@@ -4,6 +4,7 @@ $(function() {
 
 	var gioia = [16.9299694, 40.8030459];
 	var valenzano = [16.8666082, 41.0458656];
+	var gravina = [16.4074053, 40.8183414];
 
 	/*
 	var map = L.Mapzen.map('map');
@@ -43,6 +44,7 @@ $(function() {
             "properties": {
                 "title": "Gioia del Colle",
                 "iconSize": [60,60]
+                "rss": "https://gioialab.github.io/feed.xml"
             }
         }, {
             "type": "Feature",
@@ -52,9 +54,23 @@ $(function() {
             },
             "properties": {
                 "title": "Valenzano",
-                "iconSize": [48,48]
+                "iconSize": [60,60]
+                "rss": ""
             }
-        }]
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": gravina
+            },
+            "properties": {
+                "title": "Gravina in Puglia",
+                "iconSize": [60,60]
+                "rss": ""
+            }
+        }
+        ]
     };
 
     // add markers to map
@@ -67,14 +83,64 @@ $(function() {
 	    el.style.height = marker.properties.iconSize[1] + 'px';
 
 	    el.addEventListener('click', function() {
-	        window.alert(marker.properties.title);
+	    	map.flyTo({center: marker.geometry.coordinates});
+	        // window.alert(marker.properties.title);
 	    });
-
+	    
 	    // add marker to map
 	    new mapboxgl.Marker(el, {offset: [-marker.properties.iconSize[0] / 2, -marker.properties.iconSize[1] / 2]})
 	        .setLngLat(marker.geometry.coordinates)
 	        .addTo(map);
 	});
+
+	map.on('load', function () {
+
+	    // add geojson data as a new source
+	    map.addSource("symbols", {
+	        "type": "geojson",
+	        "data": geojson
+	    });
+
+	    map.addLayer({
+	        "id": "symbols",
+	        "type": "symbol",
+	        "source": "symbols",
+	        "paint": {}
+	    });
+
+	    map.on('click', function (e) {
+		    // Use queryRenderedFeatures to get features at a click event's point
+		    // Use layer option to avoid getting results from other layers
+		    var features = map.queryRenderedFeatures(e.point, { layers: ['symbols'] });
+		    // if there are features within the given radius of the click event,
+		    // fly to the location of the click event
+		    if (features.length) {
+		        // Get coordinates from the symbol and center the map on those coordinates
+		        map.flyTo({center: features[0].geometry.coordinates});
+		    }
+		});
+
+		map.on('mousemove', function (e) {
+		    var features = map.queryRenderedFeatures(e.point, { layers: ['symbols'] });
+		    map.getCanvas().style.cursor = features.length ? 'pointer' : '';
+		});
+
+	});
+
+	function getRSS(feed) {
+
+		$.get(feed, function (data) {
+		    $(data).find("entry").each(function () { // or "item" or whatever suits your feed
+		        var el = $(this);
+
+		        console.log("------------------------");
+		        console.log("title      : " + el.find("title").text());
+		        console.log("author     : " + el.find("author").text());
+		        console.log("description: " + el.find("description").text());
+		    });
+		});
+
+	}
 
 
 });
--- _site/js/rss.js
+++ _site/js/rss.js
@@ -1,11 +1,4 @@
 $(function() {
 
-	var center = [40.8030459,16.9299694];
 
-	var map = L.Mapzen.map('map');
-  	// Set the center of the map to be the San Francisco Bay Area at zoom level 12
-  	map.setView(center, 8);
-
-
-
-});
+})
